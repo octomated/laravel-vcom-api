cb0693bb0e5044042fb2aa5686984af75a1f020e
Philipp Wegner <1503971-philuxx@users.noreply.gitlab.com>
2024-04-01 16:36:32 +0200

    Define missing methods

--- src/VcomApi.php
+++ src/VcomApi.php
@@ -3,10 +3,24 @@
 namespace Octomated\VcomApi;
 
 use meteocontrol\client\vcomapi\ApiClient;
+use meteocontrol\client\vcomapi\endpoints\main\Alarms;
+use meteocontrol\client\vcomapi\endpoints\main\Session;
 use meteocontrol\client\vcomapi\endpoints\main\Systems;
+use meteocontrol\client\vcomapi\endpoints\main\Tickets;
+use meteocontrol\client\vcomapi\endpoints\sub\alarms\Alarm;
+use meteocontrol\client\vcomapi\endpoints\sub\cmms\Cmms;
+use meteocontrol\client\vcomapi\endpoints\sub\systems\System;
+use meteocontrol\client\vcomapi\endpoints\sub\tickets\Ticket;
 
 /**
  * @method Systems systems()
+ * @method System system(string $systemKey)
+ * @method Tickets tickets()
+ * @method Ticket ticket(string $ticketId)
+ * @method Alarms alarms()
+ * @method Alarm alarm(int $alarmId)
+ * @method Session session()
+ * @method Cmms cmms()
  */
 readonly class VcomApi
 {
